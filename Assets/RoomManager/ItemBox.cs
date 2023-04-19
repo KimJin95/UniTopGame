71bda41c5d6f89b12efdf9c2724add2ade29e409
KimJin95 <129369841+KimJin95@users.noreply.github.com>
2023-04-19 14:27:04 +0900

    itemBox 스크립트 생성
    player와 닿으면 열리는 이미지로 변경 후 아이템 생성(키)

---
+++ Assets/RoomManager/ItemBox.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//make item
+//change open image
+
+public class ItemBox : MonoBehaviour
+{
+    [SerializeField] GameObject itemPrefab;
+    [SerializeField] Sprite openImage;
+
+
+    bool isClosed = true;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (isClosed && other.transform.CompareTag("Player"))
+        {
+            isClosed = false;
+
+            if (itemPrefab != null)
+            {
+                Instantiate(itemPrefab, transform.position, Quaternion.identity);
+            }
+
+            GetComponent<SpriteRenderer>().sprite = openImage;
+        }
+    }
+
+}
